8c10a810ea2dceff88026cc6e47bf9383bb1bf08
Koen Claessen <koen@chalmers.se>
2009-03-23 12:56:32 +0000

    fixat Serial.hs

---
+++ Haskell/Infinox/Types.hs
@@ -0,0 +1,7 @@
+module Infinox.Types where
+
+import Form
+
+type Function  = Term -- has one variable X
+type Relation  = Form -- has two variables X, Y
+type Predicate = Form -- has one variable X
